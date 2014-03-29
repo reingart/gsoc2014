e4fc78617ab2e26246f36fb2e2c8c7af692d340f
Mariano Reingart <reingart@gmail.com>
2014-03-29 15:10:01 -0300

    improvements to debug script: fixed solib search path (now it finds almost all the libraries), removed hard-coded paths, better comments

--- android/gdb.sh
+++ android/gdb.sh
@@ -6,9 +6,15 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 #
 
-# configure the NDK path
+# configure your paths
 
-ANDROID_NDK_ROOT=~/src/android-ndk-r9d
+PROGDIR=`dirname $0`
+PROGDIR=`cd $PROGDIR && pwd -P`
+HOME=/home/reingart
+ANDROID_NDK_ROOT=$HOME/src/android-ndk-r9d/
+ANDROID_SDK_ROOT=$HOME/src/android-sdk-linux/
+QT_ROOT=$HOME/qt
+WX_QT_BUILD=$HOME/src/wxQT/android
 AWK_SCRIPTS=$ANDROID_NDK_ROOT/build/awk
 GDB_CLIENT=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gdb
 
@@ -18,6 +24,8 @@ run ()
     echo
     "$@" 2>&1
 }
+
+# first get the application name to debug:
 
 PACKAGE_NAME=`awk -f $AWK_SCRIPTS/extract-package-name.awk AndroidManifest.xml`
 OPTION_LAUNCH=`awk -f $AWK_SCRIPTS/extract-launchable.awk AndroidManifest.xml`
@@ -31,21 +39,18 @@ OPTION_LAUNCH=`awk -f $AWK_SCRIPTS/extract-launchable.awk AndroidManifest.xml`
 run adb shell am start -n $PACKAGE_NAME/$OPTION_LAUNCH
 run adb shell sleep 2
 
-# getpid and datadir:
+# get the PID and data dir from the running app:
 
 #run adb shell ps
 PID=`adb shell ps | awk -f $AWK_SCRIPTS/extract-pid.awk -v PACKAGE="$PACKAGE_NAME"`
 DATA_DIR=/data/data/$PACKAGE_NAME #`adb shell run-as $PACKAGE_NAME /system/bin/sh -c pwd`
 DEVICE_GDBSERVER=$DATA_DIR/lib/gdbserver
 
-#echo PID=$PID DATA_DIR=$DATA_DIR DEV_GDB=$DEVICE_GDBSERVER
-#echo "------------------------------------------------------------------------------------"
-
 # start debugger server
 
 run adb shell run-as $PACKAGE_NAME $DEVICE_GDBSERVER +debug-socket --attach $PID &
 
-# Get the app_server binary from the device
+# Get the app_server binary and libraries from the device
 
 APP_OUT=./libs/armeabi/
 APP_PROCESS=$APP_OUT/app_process
@@ -53,8 +58,13 @@ APP_PROCESS=$APP_OUT/app_process
 echo "Pulled app_process from device/emulator."
 #adb pull /system/bin/linker $APP_OUT/linker
 echo "Pulled linker from device/emulator."
-#adb pull /system/lib/libc.so $APP_OUT/libc.so
+adb pull /system/lib/libc.so $APP_OUT/libc.so
 echo "Pulled libc.so from device/emulator."
+if [ ! -d /tmp/system_lib ]
+then
+    adb pull /system/lib /tmp/system_lib
+    echo "Pulled system lib folder from device/emulator."
+fi
 
 # forward port
 
@@ -67,12 +77,20 @@ run adb forward tcp:1234 localfilesystem:$DATA_DIR/debug-socket
 #jdb -connect com.sun.jdi.SocketAttach:hostname=localhost,port=$JDB_PORT &
 sleep 1
 
+# create sourced command file (similar to the one created by ndk-build):
 
-echo "set solib-search-path $APP_OUT " > gdb.setup
-echo "directory /home/reingart/src/android-ndk-r9d/platforms/android-9/arch-arm/usr/include /home/reingart/src/android-ndk-r9d/sources/android/native_app_glue .. /home/reingart/src/android-ndk-r9d/sources/cxx-stl/system /home/reingart/src/android-ndk-r9d/platforms/android-9/arch-arm/usr/lib/" >> gdb.setup 
+echo "target remote :1234" > gdb.setup
 echo "file $APP_PROCESS" >> gdb.setup 
-echo "target remote :1234" >> gdb.setup
-
+echo -n "set solib-search-path $QT_ROOT/5.2.1/android_armv5/lib/:" >> gdb.setup
+echo -n "$ANDROID_NDK_ROOT/platforms/android-10/arch-arm/usr/lib:" >> gdb.setup
+echo -n "/tmp/system_lib:" >> gdb.setup
+echo -n "/tmp/system_lib/hw:" >> gdb.setup  # this one for gralloc.default.so
+echo    "$PROGDIR/$APP_OUT" >> gdb.setup
+echo "directory $ANDROID_NDK_ROOT/platforms/android-9/arch-arm " >> gdb.setup
+echo "directory $WX_QT_BUILD " >> gdb.setup
+echo "sharedlibrary" >> gdb.setup
+echo "set auto-solib-add on" >> gdb.setup
+ 
 # start debugger client
 $GDB_CLIENT -x gdb.setup 
 
